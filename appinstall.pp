0b6f56220d2256f17d9e0f8985476957c8f3d966
Artem Yakimenko <code@temik.me>
2015-02-05 20:59:49 +0100

    Switching to brew-cask to do all the heavy lifting

--- appinstall.pp
+++ appinstall.pp
@@ -1,80 +1,14 @@
-# Install frequently used mac applications
+##TODO: Fix homebrew setup
+include homebrew
 
-define app_deploy_dmg($sourcedir = false)
-{
-  $sourcedir_real = $sourcedir ? {
-    false => "http://puppet.reductivelabs.foo/osx/pkgs/apps",
-    default => $sourcedir
-  }
-  package { $name:
-    ensure => installed,
-    provider => appdmg,
-    source => "$sourcedir_real/$name"
-  }
-}
+# class {'homebrew':
+#   user  => temikus,
+#   group => admin,
+# }
 
-define app_deploy_pkg($sourcedir = false)
-{
-  $sourcedir_real = $sourcedir ? {
-    false => "http://puppet.reductivelabs.foo/osx/pkgs/apps",
-    default => $sourcedir
-  }
-  package { $name:
-    ensure => installed,
-    provider => pkgdmg,
-    source => "$sourcedir_real/$name"
-  }
-}
+package { 'wget': provider => brew }
+package { 'autojump': provider => brew }
+package { 'mtr': provider => brew }
 
 
-#####################################################################
-#TODO: Fix appzip provider
-#####################################################################
-
-define app_deploy_appzip($sourcedir = false)
-{
-  $sourcedir_real = $sourcedir ? {
-    false => "http://puppet.reductivelabs.foo/osx/pkgs/apps",
-    default => $sourcedir
-  }
-  package { $name:
-    ensure => installed,
-    provider => appzip,
-    source => "$sourcedir_real/$name"
-  }
-}
-
-app_deploy_appzip { "colloquy-latest.zip":
-            sourcedir => "http://colloquy.info/downloads",
-            alias => colloquy
-          }
-
-#####################################################################
-
-# Example
-#
-# app_deploy_dmg { "Firefox%2034.0.5.dmg":
-#             sourcedir => "https://download-installer.cdn.mozilla.net/pub/firefox/releases/34.0.5/mac/en-US",
-#             alias => firefox
-#           }
-
-app_deploy_dmg { "TextExpander.dmg":
-            sourcedir => "https://dl.dropboxusercontent.com/u/334637/Software",
-            alias => textexpander
-          }
-
-app_deploy_dmg { "SourceTree_2.0.4.dmg":
-            sourcedir => "http://downloads.atlassian.com/software/sourcetree",
-            alias => sourcetree }
-
-app_deploy_dmg { "GoogleChrome.dmg":
-            sourcedir => "https://dl.google.com/chrome/mac/dev",
-            alias => googlechrome }
-
-app_deploy_dmg { "Sublime%20Text%20Build%203065.dmg":
-            sourcedir => "http://c758482.r82.cf2.rackcdn.com",
-            alias => sublimetext }
-
-app_deploy_dmg { "installgoogledrive.dmg":
-            sourcedir => "https://dl.google.com/drive",
-            alias => installgoogledrive }
+package { 'adium': provider => 'brewcask' }
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,50 +1,142 @@
-#!/bin/bash
+#!/bin/bash -x
+
+# Parse options
+for opt in "$@"
+do
+    case "$opt" in
+    -h|--help)
+        show_help
+        exit 0
+        ;;
+    -v|--verbose)
+				verbose=1
+        ;;
+    --with-puppet)
+				with_puppet=true
+				;;
+		--without-homebrew)
+				without_homebrew=true
+    		;;
+    esac
+done
+
+# Default settings
+with_puppet=${with_puppet:-false}
+without_homebrew=${without_homebrew:-false}
+sleep_timeout=5
+
+debug()
+{
+	[ "$verbose" ] && echo ">>> $*";
+}
+
+timeout()
+{
+	#Regulates timeout after output-intensive commands
+	sleep $timeout
+}
+
+show_help()
+{
+
+  echo "Usage: `basename $0` [-h|--help] [-v|--verbose] [options]"
+	echo ""
+	echo "Options:"
+	echo "--with-puppet - Install puppet client on the machine"
+
+  exit 0
+}
 
 #Cleaning up
 cleanup()
 {
-rm /tmp/puppet-*.dmg
-hdiutil unmount /Volumes/puppet-*
-rm /tmp/facter-*.dmg
-hdiutil unmount /Volumes/facter-*
+	rm /tmp/puppet-*.dmg
+	hdiutil unmount /Volumes/puppet-*
+	rm /tmp/facter-*.dmg
+	hdiutil unmount /Volumes/facter-*
 }
 
 download_puppet()
 {
-curl -o /tmp/puppet-latest.dmg https://downloads.puppetlabs.com/mac/puppet-latest.dmg
-curl -o /tmp/facter-2.4.0.dmg https://downloads.puppetlabs.com/mac/facter-2.4.0.dmg
+	curl -o /tmp/puppet-latest.dmg https://downloads.puppetlabs.com/mac/puppet-latest.dmg
+}
+
+download_facter()
+{
+	curl -o /tmp/facter-2.4.0.dmg https://downloads.puppetlabs.com/mac/facter-2.4.0.dmg
 }
 
 install_puppet()
 {
-hdiutil mount /tmp/puppet-latest.dmg
-hdiutil mount /tmp/facter-2.4.0.dmg
-sudo installer -pkg /Volumes/puppet-*/puppet-*.pkg -target /
+	hdiutil mount /tmp/puppet-latest.dmg
+	hdiutil mount /tmp/facter-2.4.0.dmg
+	sudo installer -pkg /Volumes/puppet-*/puppet-*.pkg -target /
 }
 
 install_facter()
 {
-hdiutil mount /tmp/facter-2.4.0.dmg
-sudo installer -pkg /Volumes/facter-*/facter-*.pkg -target /
+	hdiutil mount /tmp/facter-2.4.0.dmg
+	sudo installer -pkg /Volumes/facter-*/facter-*.pkg -target /
 }
 
-install_packages()
+run_puppet_manifests()
 {
-sudo puppet apply --debug --modulepath=./modules appinstall.pp
+	echo "Running puppet manifests..."
+	puppet apply --debug --modulepath=./modules appinstall.pp
 }
 
-if [[ ! -x /usr/bin/puppet ]]; then
-  cleanup
-  download_puppet
-  install_puppet
+install_homebrew()
+{
+	echo "Installing Homebrew..."
+	Using the standard ruby install pipe.
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+	timeout
+}
+
+install_homebrew_packages()
+{
+	brew install wget mtr autojump zsh-syntax-highlighting
+}
+
+install_cask()
+{
+	brew install caskroom/cask/brew-cask
+	brew tap caskroom/versions
+}
+
+install_cask_packages()
+{
+	brew cask install google-chrome iterm2-beta skype alfred
+}
+
+debug "Launching with settings:"
+debug "with_puppet=$with_puppet"
+debug "without_homebrew=$without_homebrew"
+
+#Install puppet if flag is supplied
+if [[ $with_puppet == true ]]; then
+
+	if [[ ! -x /usr/bin/puppet ]]; then
+	  cleanup
+	  download_puppet
+	  install_puppet
+	fi
+
+	if [[ ! -x /usr/bin/facter ]]; then
+	  cleanup
+	  download_puppet
+	  install_facter
+	fi
+	run_puppet_manifests
 fi
 
-if [[ ! -x /usr/bin/facter ]]; then
-  cleanup
-  download_puppet
-  install_facter
+if [[ $without_homebrew == false ]]; then
+  install_homebrew
+  install_homebrew_packages
 fi
 
-install_packages
 
+echo "All done!"
 
+exit 0
