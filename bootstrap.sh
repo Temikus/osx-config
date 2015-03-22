b1241abd5a6c4d43721a91c57e6c4afcc87fb69d
Artem Yakimenko <code@temik.me>
2015-03-22 11:12:17 +0100

    Tabs to spaces

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,14 +9,14 @@ do
         exit 0
         ;;
     -v|--verbose)
-				verbose=1
+      verbose=1
         ;;
     --with-puppet)
-				with_puppet=true
-				;;
-		--without-homebrew)
-				without_homebrew=true
-    		;;
+      with_puppet=true
+        ;;
+    --without-homebrew)
+        without_homebrew=true
+        ;;
     esac
 done
 
@@ -28,7 +28,7 @@ puppet_modules_location=${puppet_modules_location:-"./puppet/modules"}
 
 debug()
 {
-	[ "$verbose" ] && echo ">>> $*";
+  [ "$verbose" ] && echo ">>> $*";
 }
 
 #################################
@@ -52,17 +52,17 @@ function yellow() {
 
 timeout()
 {
-	#Regulates timeout after output-intensive commands
-	sleep 5
+  #Regulates timeout after output-intensive commands
+  sleep 5
 }
 
 show_help()
 {
   echo "Usage: `basename $0` [-h|--help] [-v|--verbose] [options]"
-	echo ""
-	echo "Options:"
-	echo "--with-puppet - Install puppet client on the machine"
-	echo "--without-homebrew - Do not set up homebrew or homebrew-cask"
+  echo ""
+  echo "Options:"
+  echo "--with-puppet - Install puppet client on the machine"
+  echo "--without-homebrew - Do not set up homebrew or homebrew-cask"
 
   exit 0
 }
@@ -70,81 +70,81 @@ show_help()
 #Cleaning up
 cleanup()
 {
-	rm /tmp/puppet-*.dmg
-	hdiutil unmount /Volumes/puppet-*
-	rm /tmp/facter-*.dmg
-	hdiutil unmount /Volumes/facter-*
+  rm /tmp/puppet-*.dmg
+  hdiutil unmount /Volumes/puppet-*
+  rm /tmp/facter-*.dmg
+  hdiutil unmount /Volumes/facter-*
 }
 
 download_puppet()
 {
-	curl -o /tmp/puppet-latest.dmg https://downloads.puppetlabs.com/mac/puppet-latest.dmg
+  curl -o /tmp/puppet-latest.dmg https://downloads.puppetlabs.com/mac/puppet-latest.dmg
 }
 
 download_facter()
 {
-	curl -o /tmp/facter-2.4.0.dmg https://downloads.puppetlabs.com/mac/facter-2.4.0.dmg
+  curl -o /tmp/facter-2.4.0.dmg https://downloads.puppetlabs.com/mac/facter-2.4.0.dmg
 }
 
 install_puppet()
 {
-	hdiutil mount /tmp/puppet-latest.dmg
-	hdiutil mount /tmp/facter-2.4.0.dmg
-	sudo installer -pkg /Volumes/puppet-*/puppet-*.pkg -target /
+  hdiutil mount /tmp/puppet-latest.dmg
+  hdiutil mount /tmp/facter-2.4.0.dmg
+  sudo installer -pkg /Volumes/puppet-*/puppet-*.pkg -target /
 }
 
 install_facter()
 {
-	hdiutil mount /tmp/facter-2.4.0.dmg
-	sudo installer -pkg /Volumes/facter-*/facter-*.pkg -target /
+  hdiutil mount /tmp/facter-2.4.0.dmg
+  sudo installer -pkg /Volumes/facter-*/facter-*.pkg -target /
 }
 
 run_puppet_manifests()
 {
-	yellow "Running puppet manifests..."
-	puppet apply --debug --modulepath=$puppet_modules_location $puppet_manifest_location
+  yellow "Running puppet manifests..."
+  puppet apply --debug --modulepath=$puppet_modules_location $puppet_manifest_location
 }
 
 install_homebrew()
 {
-	yellow "Installing Homebrew..."
-	#Using the standard ruby install pipe.
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  yellow "Installing Homebrew..."
+  #Using the standard ruby install pipe.
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-	timeout
+  timeout
 }
 
 install_homebrew_packages()
 {
-	yellow "Installing homebrew packages..."
-	brew install wget mtr autojump zsh-syntax-highlighting ack
+  yellow "Installing homebrew packages..."
+  brew install wget mtr autojump zsh-syntax-highlighting ack
 }
 
 install_cask()
 {
-	brew install caskroom/cask/brew-cask
-	brew tap caskroom/versions
+  brew install caskroom/cask/brew-cask
+  brew tap caskroom/versions
 }
 
 install_cask_packages()
 {
-	yellow "Installing cask..."
-	# TODO: Make a configuration for the list of apps in the beginning
-	brew cask install --appdir=/Applications google-chrome iterm2-beta skype alfred sublime-text3 dropbox google-drive flux
+  yellow "Installing cask..."
+  # TODO: Make a configuration for the list of apps in the beginning
+  brew cask install --appdir=/Applications google-chrome iterm2-beta skype alfred sublime-text3 dropbox google-drive flux
 
-	timeout
+  timeout
 
-	yellow "Copying apps to /Applications..."
-	# We're piggybacking off cask to install the necessary apps and get the latest versions
+  yellow "Copying apps to /Applications..."
+  # We're piggybacking off cask to install the necessary apps and get the latest versions
   find /opt/homebrew-cask/Caskroom -name "*.app" -type d -depth 3 | while read app_file_path; do
-  	cp -r "$app_file_path" /Applications
+    cp -r "$app_file_path" /Applications
   done
 }
 
 git_setup()
 {
 
-	yellow "Setting up git..."
+  yellow "Setting up git..."
 
   git config --global user.name "Artem Yakimenko"
   git config --global user.email code@temik.me
@@ -155,26 +155,26 @@ git_setup()
 
 copy_oh_my_zsh_custom()
 {
-	yellow "Copying oh-my-zsh custom configs..."
+  yellow "Copying oh-my-zsh custom configs..."
   cp -r ./oh-my-zsh/* ~/.oh-my-zsh/custom/ || { red 'Could not init submodules' ; exit 1; }
 }
 
 update_git_submodules()
 {
 
-	yellow "Setting up git submodules..."
+  yellow "Setting up git submodules..."
 
-	git submodule init || { red 'Could not init submodules' ; exit 1; }
-	git submodule update || { red 'Could not update submodules' ; exit 1; }
+  git submodule init || { red 'Could not init submodules' ; exit 1; }
+  git submodule update || { red 'Could not update submodules' ; exit 1; }
 }
 
 setup_sublime_text()
 {
 
-	yellow "Copying sublime user config..."
-	cp ./configs/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+  yellow "Copying sublime user config..."
+  cp ./configs/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
-	yellow "Setting up Sublime puppet module..."
+  yellow "Setting up Sublime puppet module..."
   mkdir ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/SublimePuppet
   git clone https://github.com/russCloak/SublimePuppet.git ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/SublimePuppet
 
@@ -182,8 +182,8 @@ setup_sublime_text()
 
 setup_defaults()
 {
-	yellow "Setting up Mac defaults. This will require your password..."
-	./configs/defaults_mac.sh
+  yellow "Setting up Mac defaults. This will require your password..."
+  ./configs/defaults_mac.sh
 }
 
 debug "Launching with settings:"
@@ -200,18 +200,18 @@ copy_oh_my_zsh_custom
 #Install puppet if flag is supplied
 if [[ $with_puppet == true ]]; then
 
-	if [[ ! -x /usr/bin/puppet ]]; then
-	  cleanup
-	  download_puppet
-	  install_puppet
-	fi
+  if [[ ! -x /usr/bin/puppet ]]; then
+    cleanup
+    download_puppet
+    install_puppet
+  fi
 
-	if [[ ! -x /usr/bin/facter ]]; then
-	  cleanup
-	  download_puppet
-	  install_facter
-	fi
-	run_puppet_manifests
+  if [[ ! -x /usr/bin/facter ]]; then
+    cleanup
+    download_puppet
+    install_facter
+  fi
+  run_puppet_manifests
 fi
 
 if [[ $without_homebrew == false ]]; then
